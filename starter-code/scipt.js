6b7bf3b932af556949de16c3020679fd9fbdb57a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-01-17 20:25:56 +0100

    script completato e funzionante

--- starter-code/scipt.js
+++ starter-code/scipt.js
@@ -2,38 +2,40 @@
 const newParagraph = document.createElement('p');
 document.getElementById('email-check').appendChild(newParagraph);
 
-
 document.getElementById('form').addEventListener('submit', (event) => {
 
     event.preventDefault();
 
-    const emailValue = document.forms['form'].email.value;
+    const emailValue = document.forms['form'].email.value; // valore email da passare alla funzione di validazione
 
-    const isEmailValid = validazioneEmail(emailValue);
+    const isEmailValid = emailValidate(emailValue); // ritorno del responso della funzione di validazione
 
-    if (!isEmailValid) {
-        emailInvalid();
-    } else {
-        emailValid();
-    }
+    // if (!isEmailValid) {
+    //     emailInvalid();
+    // } else {
+    //     emailValid();
+    // }
+    if(!isEmailValid ? emailInvalid() : emailValid()); // molto più semplice e comprensibile oltre che mi piace di più
 });
 
+// se email valida scive questo paragrafo
 function emailValid() {
-    newParagraph.textContent = "Form submitted successfully";
+    newParagraph.textContent = "Form submitted successfully ;)";
     newParagraph.style.color = "green";
     console.log("si fa per dire ;)");
 }
 
+// se email non valida scive questo paragrafo
 function emailInvalid() {
     newParagraph.textContent = "Oops! Please check your email";
 }
 
-function validazioneEmail(email) {
+// validatore di email
+function emailValidate(email) {
     if (email == '') {
         // console.log("sono passato di qui");
         return false;
     }
-
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         return true;  // Restituisci true se l'email è valida
     } else {
